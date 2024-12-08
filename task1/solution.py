1f70485dfefe852cb6f6d4d773324a523966d790
KirGorbunov <KirGorbunov@gmail.com>
2024-12-08 12:41:42 +0300

    Change logic for args

--- task1/solution.py
+++ task1/solution.py
@@ -4,13 +4,12 @@ from inspect import get_annotations
 def strict(func):
     def wrapper(*args, **kwargs):
         annotations = get_annotations(func)
-        args_list = list(args)
-        for i in range(len(args_list)):
-            if type(args_list[i]) is not list(annotations.values())[i]:
-                raise TypeError(f"Аргумент {i+1} имеет тип {type(args_list[i])}, что не соответствует аннотации")
-        for j in kwargs:
-            if type(kwargs[j]) is not annotations[j]:
-                raise TypeError(f"Аргумент {j} имеет тип {type(kwargs[j])}, что не соответствует аннотации")
+        for (param, value) in zip(annotations.keys(), args):
+            if type(value) is not annotations.get(param):
+                raise TypeError(f"Аргумент {param} имеет тип {type(value)}, что не соответствует аннотации")
+        for kwarg in kwargs:
+            if type(kwargs[kwarg]) is not annotations[kwarg]:
+                raise TypeError(f"Аргумент {kwarg} имеет тип {type(kwargs[kwarg])}, что не соответствует аннотации")
         result = func(*args, **kwargs)
         return result
     return wrapper
--- task1/test.py
+++ task1/test.py
@@ -6,12 +6,12 @@ assert sum_two(2, 3) == 5
 try:
     sum_two(2, "3")
 except TypeError as e:
-    assert str(e) == "Аргумент 2 имеет тип <class 'str'>, что не соответствует аннотации"
+    assert str(e) == "Аргумент b имеет тип <class 'str'>, что не соответствует аннотации"
 
 try:
     sum_two("2", 3)
 except TypeError as e:
-    assert str(e) == "Аргумент 1 имеет тип <class 'str'>, что не соответствует аннотации"
+    assert str(e) == "Аргумент a имеет тип <class 'str'>, что не соответствует аннотации"
 
 assert sum_two(b=2, a=3) == 5
 
@@ -30,7 +30,7 @@ assert sum_two(2, b=3) == 5
 try:
     sum_two("2", b=3)
 except TypeError as e:
-    assert str(e) == "Аргумент 1 имеет тип <class 'str'>, что не соответствует аннотации"
+    assert str(e) == "Аргумент a имеет тип <class 'str'>, что не соответствует аннотации"
 
 try:
     sum_two(2, b="3")
